db0fdd1c96f9e1761e6431086db57f842f43b954
Laurent Binder <laurentbinder@gmail.com>
2020-04-25 12:35:13 +0200

    changed js logo's

--- app/javascript/plugins/init_letters.js
+++ app/javascript/plugins/init_letters.js
@@ -1,39 +1,20 @@
+// Wrap every letter in a span
 const initLetters = () => {
   var textWrapper = document.querySelector('.moving-letters .letters');
   textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
   anime.timeline({loop: false})
     .add({
-      targets: '.moving-letters .line',
-      opacity: [0.5,1],
-      scaleX: [0, 1],
-      easing: "easeInOutExpo",
-      duration: 700
+      targets: '.moving-letters .letter',
+      rotateY: [-90, 0],
+      duration: 1300,
+      delay: (el, i) => 45 * i
     }).add({
-      targets: '.moving-letters .line',
-      duration: 600,
+      targets: '.moving-letters',
+      opacity: 1,
+      duration: 1000,
       easing: "easeOutExpo",
-      translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
-    }).add({
-      targets: '.moving-letters .ampersand',
-      opacity: [0,1],
-      scaleY: [0.5, 1],
-      easing: "easeOutExpo",
-      duration: 600,
-      offset: '-=600'
-    }).add({
-      targets: '.moving-letters .letters-left',
-      opacity: [0,1],
-      translateX: ["0.5em", 0],
-      easing: "easeOutExpo",
-      duration: 600,
-      offset: '-=300'
-    }).add({
-      targets: '.moving-letters .letters-right',
-      opacity: [0,1],
-      translateX: ["-0.5em", 0],
-      easing: "easeOutExpo",
-      duration: 600,
-      offset: '-=600'
+      delay: 1000
     });
 }
 
