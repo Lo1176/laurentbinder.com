abacd828fe2fb0563b4ef9cd9d6846a0e4ab9e2c
Laurent Binder <laurentbinder@gmail.com>
2020-10-29 17:17:42 +0100

    banner sloglan js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import initLetters from "../plugins/init_letters";
 // import initDetailsLetters from "../plugins/init_details_letters";
 import initSrollUp from "../plugins/init_scrollup";
-import { init } from 'ityped';
+import { init } from 'ityped'
 // import initItyped from "../plugins/init_ityped";
 
 
@@ -20,6 +20,17 @@ initSrollUp();
 // letterfx is not working
 // $("#tagline").letterfx({"fx":"fly-right fly-bottom spin"});
 
-const devElement = document.querySelector('#dev')
 
-init(devElement, { showCursor: false, startDelay: 50, typeSpeed:  90, backDelay:  500, disableBackTyping: false, strings: ['Création de sites Web modernes', 'sur mesure', 'responsive', 'référencés', 'magnifico !' ] });
+// https://github.com/luisvinicius167/ityped
+const devElement = document.querySelector('#dev')
+const devSuffixElement = document.querySelector('#dev-suffix')
+function firstSlogan(){
+  init(devElement, { showCursor: false, loop: false, startDelay: 50, strings: [ 'Création de sites Web ' ]});
+}
+
+setTimeout(function suffixSlogan() {
+     init(devSuffixElement, { loop: false, typeSpeed:  90, backDelay:  500, disableBackTyping: false, strings: [ 'modernes', 'sur mesure', 'responsive', 'référencés', 'magnifico !', 'sur mesure ;)' ] });
+
+  }, 8000);
+
+// firstSlogan();
